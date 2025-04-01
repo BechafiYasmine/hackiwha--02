54472c063634a3dd98e6c5c1b985acdf3f31d371
yasminebcf <yasminebcf21@gmail.com>
2025-04-01 23:45:07 +0100

    API sign in

--- Server/server.js
+++ Server/server.js
@@ -46,6 +46,27 @@ app.post('/register', (req, res) => {
     });
 });
 
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE email = ?";
+    db.query(sql, [req.body.email], (err, data) => {
+        if(err) return res.json({Error: "Login error in server"});
+
+        if(data.length > 0) {
+            bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
+                if(err) return res.json({Error: "Password compare error"});
+
+                if(response) {
+                    return res.json({Status: "Success"});
+                } else {
+                    return res.json({Error: "Password not matched"});
+                }
+            })
+        } else {
+            return res.json({Error: "No email existed"});
+        }
+    });
+});
+
 
 db.connect((err) => {
     if (err) {
--- frontEnd/src/Login.jsx
+++ frontEnd/src/Login.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import {React , useState} from 'react'
 import {Link} from 'react-router-dom'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [values, setValues] = useState({
+    
+    email: '',
+    password: ''
+  });
+
+  const navigate = useNavigate()
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      axios.post('http://localhost:5000/login', values)
+      .then(res => {
+          if(res.data.Status === "Success") {
+              navigate('/')
+          } else {
+              alert(res.data.Error);
+          }
+      })
+      .then(err => console.log(err));
+  }
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign-In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email"><strong>Email</strong></label>
             <input 
               type="email" 
               placeholder="Enter Email" 
               name="email" 
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
               className="form-control rounded-0" 
             />
           </div>
@@ -22,6 +44,7 @@ function Login() {
               type="password" 
               placeholder="Enter Password" 
               name="password" 
+              onChange={(e) => setValues({ ...values, password: e.target.value })}
               className="form-control rounded-0" 
             />
           </div>
